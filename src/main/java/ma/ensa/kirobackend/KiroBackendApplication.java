42a455daad196bb73fac8c71cd8172973bbeb3d5
hassaneelhariti <hassaneelhariti@gmail.com>
2025-12-13 02:57:47 +0100

    entities mapping

---
+++ src/main/java/ma/ensa/kirobackend/KiroBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.ensa.kirobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiroBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/ensa/kirobackend/KiroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ensa.kirobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
