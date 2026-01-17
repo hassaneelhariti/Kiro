5b015a2b78d01367eda5e4e621815712e5f6ead9
ousam713 <ousambenz007@gmail.com>
2026-01-17 18:44:39 +0100

    implement : ProjectServiceImpl.getProject & reimplement : ProjetMapper

--- src/main/java/ma/ensa/kirobackend/mappers/ProjetMapper.java
+++ src/main/java/ma/ensa/kirobackend/mappers/ProjetMapper.java
@@ -2,10 +2,22 @@ package ma.ensa.kirobackend.mappers;
 
 import ma.ensa.kirobackend.dtos.ProjetDto;
 import ma.ensa.kirobackend.entities.Projet;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+@Mapper(componentModel = "spring")
 public interface ProjetMapper {
 
+    @Mapping(target = "productOwnerId", source = "productOwner.id")
+    @Mapping(target = "scrumMasterId", source = "scrumMaster.id")
+    @Mapping(target = "productBacklogId", source = "productBacklog.id")
+    @Mapping(target = "sprints", source = "sprintBacklogs")
     ProjetDto toDto(Projet projet);
 
+    @Mapping(target = "productOwner", ignore = true)
+    @Mapping(target = "scrumMaster", ignore = true)
+    @Mapping(target = "productBacklog", ignore = true)
+    @Mapping(target = "developers", ignore = true)
+    @Mapping(target = "sprintBacklogs", ignore = true)
     Projet toProjet(ProjetDto projetDto);
 }
