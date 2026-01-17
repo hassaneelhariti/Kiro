70d1ad8c3b9e17e05ddd77cdcaafdbff018f5341
ousam713 <ousambenz007@gmail.com>
2026-01-17 18:01:06 +0100

    implement all interfaces

--- src/main/java/ma/ensa/kirobackend/service/productownerservice/EpicService.java
+++ src/main/java/ma/ensa/kirobackend/service/productownerservice/EpicService.java
@@ -1,4 +1,13 @@
 package ma.ensa.kirobackend.service.productownerservice;
 
+import ma.ensa.kirobackend.dtos.EpicDto;
+
 public interface EpicService {
+
+    EpicDto getEpic(Long id);
+
+    EpicDto createEpic(EpicDto epicDto);
+
+    void deleteEpic(Long id);
+
 }
