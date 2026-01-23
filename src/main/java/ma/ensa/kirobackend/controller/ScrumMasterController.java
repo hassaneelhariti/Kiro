7b67d8a28df8945c0650f2fcbb1379791be68fb2
Hassane Elhariti <hassaneelhariti@gmail.com>
2026-01-24 00:52:18 +0100

    adding request mapping for scrum-master mappers

--- src/main/java/ma/ensa/kirobackend/controller/ScrumMasterController.java
+++ src/main/java/ma/ensa/kirobackend/controller/ScrumMasterController.java
@@ -5,36 +5,35 @@ import lombok.AllArgsConstructor;
 import ma.ensa.kirobackend.dtos.SprintBacklogDto;
 import ma.ensa.kirobackend.dtos.TaskDto;
 import ma.ensa.kirobackend.service.ScrumMasterService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/scrum-master")
+
 public class ScrumMasterController {
     private ScrumMasterService scrumMasterService;
 
 
     // create sprint
-    @PostMapping("scrum-master/{id}/create-sprint")
+    @PostMapping("/{id}/create-sprint")
     public SprintBacklogDto createSprint(@RequestBody SprintBacklogDto sprintBacklogDto){
         return scrumMasterService.saveSprint(sprintBacklogDto);
     }
     // Start / end sprint (change sprintStatus)
-    @PutMapping("scrum-master/{id}/update-sprint")
+    @PutMapping("/{id}/update-sprint")
     public void updateSprintStatus(@RequestBody SprintBacklogDto sprintBacklogDto){
         scrumMasterService.updateSprintStatus(sprintBacklogDto);
     }
 
     //Assign tasks to developers (create a task and assign it to a developer)
-    @PutMapping("scrum-master/{id}/assigned-task")
+    @PutMapping("/{id}/assigned-task")
     public  TaskDto assignTask(@RequestBody TaskDto taskDto){
         return scrumMasterService.assignTaskToDev(taskDto);
     }
 
     //Move user stories to sprint backlog
-    @PutMapping("scrum-master/{id}/link-us")
+    @PutMapping("/{id}/link-us")
     public SprintBacklogDto linkUsToSprint(@RequestBody SprintBacklogDto sprintBacklogDto){
 
         return scrumMasterService.linkUsToSprint(sprintBacklogDto) ;
